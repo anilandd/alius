bec413aa656732cd1fb73cb2f2c7e6dded975b22
Anastasia Rudenko <aniland.helpers@gmail.com>
2021-03-22 21:38:24 +0200

    thumbs up detection done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose";
 import Webcam from "react-webcam";
@@ -6,12 +6,17 @@ import Webcam from "react-webcam";
 import './App.css';
 // import { scryRenderedComponentsWithType } from 'react-dom/cjs/react-dom-test-utils.development'; // ?
 import { drawHand } from "./utilities";
+import * as fp from 'fingerpose';
+import thumbs_up from "./thumbs_up.png";
 
 
 function App() {
 
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);  
+
+  const [sign, setSign] = useState(null);
+  const images = {thumbs_up: thumbs_up};
 
   const runHandpose = async () => {
     const net = await handpose.load();
@@ -42,7 +47,27 @@ function App() {
       canvasRef.current.height =videoHeight;
       // make detections
       const hand = await net.estimateHands(video);
+      
       console.log(hand);
+      
+      // gesture detections
+      if (hand.length > 0) {
+        const GE = new fp.GestureEstimator([
+          fp.Gestures.ThumbsUpGesture
+        ])
+        const gesture = await GE.estimate(hand[0].landmarks, 8);
+        console.log(gesture);
+        if (gesture.gestures !== undefined && gesture.gestures.length > 0) {
+          const confidence = gesture.gestures.map(
+            (prediction) => prediction.confidence
+          );
+          const maxConfidence = confidence.indexOf(
+            Math.max.apply(null, confidence)
+          );
+          setSign(gesture.gestures[maxConfidence].name);
+          console.log("Gesture detected: " + sign);
+        }
+      }
       // draw mesh
       const ctx = canvasRef.current.getContext("2d");
       drawHand(hand, ctx);
@@ -54,34 +79,57 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Webcam 
-        ref={webcamRef}
-        style={{
-          position:"absolute",
-          marginLeft:"auto",
-          marginRight:"auto",
-          left:0,
-          right:0,
-          textAlign:"center",
-          zIndex:9,
-          width:640,
-          height:480
-        }}
-      />
-      <canvas
-        ref={canvasRef}
-        style={{
-          position:"absolute",
-          marginLeft:"auto",
-          marginRight:"auto",
-          left:0,
-          right:0,
-          textAlign:"center",
-          zIndex:9,
-          width:640,
-          height:480
-        }}
-      />
+        <Webcam 
+          ref={webcamRef}
+          style={{
+            position:"absolute",
+            marginLeft:"auto",
+            marginRight:"auto",
+            left:0,
+            right:0,
+            textAlign:"center",
+            zIndex:9,
+            width:640,
+            height:480
+          }}
+        />
+        <canvas
+          ref={canvasRef}
+          style={{
+            position:"absolute",
+            marginLeft:"auto",
+            marginRight:"auto",
+            left:0,
+            right:0,
+            textAlign:"center",
+            zIndex:9,
+            width:640,
+            height:480
+          }}
+        />
+        {sign !== null ? <img src={images[sign]} style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 400,
+          bottom: 500,
+          right: 0,
+          textAlign: "center",
+          height: 100,
+          zIndex:10,
+        }} />: ""}
+        {/* {sign !== null ? <div style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 400,
+          bottom: 500,
+          right: 0,
+          textAlign: "center",
+          height: 100,
+        }}>
+          Detected! 
+        </div> : ""} */}
       </header>
     </div>
   );
