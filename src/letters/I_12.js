219f7bb3bfc36be5ca3b51c2629918c286fe2251
Anastasia Rudenko <aniland.helpers@gmail.com>
2021-05-09 00:14:02 +0300

    і (8)

--- src/letters/I_12.js
+++ src/letters/I_12.js
@@ -5,31 +5,27 @@ import {Finger, FingerCurl, FingerDirection, GestureDescription} from 'fingerpos
 export const iSign12 =  new GestureDescription('i');
 
 // thumb
-iSign12.addCurl(Finger.Thumb, FingerCurl.FullCurl, 0.75);
-iSign12.addDirection(Finger.Thumb, FingerDirection.VerticalUp, 0.25);
+iSign12.addCurl(Finger.Thumb, FingerCurl.HalfCurl, 1.0);
+iSign12.addDirection(Finger.Thumb, FingerDirection.DiagonalUpLeft, 1.0);
+iSign12.addDirection(Finger.Thumb, FingerDirection.DiagonalUpRight, 1.0);
 
-// index, middle, ring
-for (let finger of [Finger.Index, Finger.Middle, Finger.Ring]) {
-    iSign12.addCurl(finger, FingerCurl.FullCurl, .75);
-    iSign12.addDirection(finger, FingerDirection.VerticalDown, 0.25); 
-}
+// index
+iSign12.addCurl(Finger.Index, FingerCurl.FullCurl, 1.0);
+iSign12.addDirection(Finger.Index, FingerDirection.DiagonalUpLeft, 1.0);
+iSign12.addDirection(Finger.Index, FingerDirection.DiagonalUpRight, 1.0);
 
-// pinky 
+// middle
+iSign12.addCurl(Finger.Middle, FingerCurl.FullCurl, 1.0);
+iSign12.addDirection(Finger.Middle, FingerDirection.DiagonalUpLeft, 1.0);
+iSign12.addDirection(Finger.Middle, FingerDirection.DiagonalUpRight, 1.0);
+
+// ring
+iSign12.addCurl(Finger.Ring, FingerCurl.FullCurl, 1.0);
+// iSign12.addDirection(Finger.Ring, FingerDirection.VerticalUp, 1.0);
+iSign12.addDirection(Finger.Ring, FingerDirection.DiagonalUpLeft, 1.0);
+iSign12.addDirection(Finger.Ring, FingerDirection.DiagonalUpRight, 1.0);
+
+// Pinky
 iSign12.addCurl(Finger.Pinky, FingerCurl.NoCurl, 1.0);
-iSign12.addDirection(Finger.Pinky, FingerDirection.VerticalUp, 0.25);
+iSign12.addDirection(Finger.Pinky, FingerDirection.VerticalUp, 1.0);
 
-
-// // thumb
-// iSign12.addCurl(Finger.Thumb, FingerCurl.NoCurl, 1.0);
-// iSign12.addDirection(Finger.Thumb, FingerDirection.HorizontalLeft, 0.25);
-// iSign12.addDirection(Finger.Thumb, FingerDirection.HorizontalRight, 0.25);
-
-// // index
-// iSign12.addCurl(Finger.Index, FingerCurl.NoCurl, 1.0);
-// iSign12.addDirection(Finger.Index, FingerDirection.VerticalUp, 0.25);
-
-// // middle, ring, pinky
-// for (let finger of [Finger.Middle, Finger.Ring, Finger.Pinky]) {
-//     iSign12.addCurl(finger, FingerCurl.FullCurl, .75);
-//     iSign12.addDirection(finger, FingerDirection.VerticalDown, 0.25); 
-// }
